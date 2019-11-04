f52d713eb8f0834f4a32722279e5e66c1e7f0154
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-04 09:41:51 -0500

    commit

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function looking(){
 
+     var name = document.getElementById("name").value;
+     var age = document.getElementById("age").value;
+     var city = document.getElementById("city").value;
+     var country = document.getElementById("country").value;
+
      var Person = {
     
-            name: "David",
-            age: 33,
-            city: "Cotacachi",
-            country: "Ecuador"
+            name: name,
+            age: age,
+            city: city,
+            country: country,
      }
      
 var myJSON = JSON.stringify(Person);
 
 document.getElementById("outPut").innerHTML = myJSON;
 
+}
+
+
+function convert(){
+
+     var Personone = document.getElementById("putJSON").value;
+     var Obj = JSON.parse(Personone);
+
+     document.getElementById("outPut1").innerHTML = "Name: " + Obj.name + "<br/>" + "Age: " + Obj.age + "<br/>" + "City: " + Obj.city + "<br/>" + "Country: " + Obj.country;
 }
